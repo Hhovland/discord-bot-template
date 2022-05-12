53ad6a8adb4e718cc9a4548e0eec19ce8afeea38
Harrison <hhovland@isoftdata.com>
2022-05-12 16:40:08 -0500

    Semiworking

---
+++ onSipMap.js
@@ -0,0 +1,33 @@
+export {
+	OnSIPMap,
+}
+
+const ISoftNum = "4024353850"
+const OnSIPMap = new Map([
+	[ "sip:isoft.menu@isoftdata.onsip.com", "Call Menu" ],
+	[ `${ISoftNum }107`, "Justin Wheeler" ],
+	[ `${ISoftNum }121`, "Holly Heffelbower" ],
+	[ "ghagemoser@isoftdata.onsip.com", "Gibran Hagemoser" ],
+	[ `${ISoftNum }130`, "James Woody" ],
+	[ "sip:ajones@isoftdata.onsip.com", "Alex Jones" ],
+	[ `${ISoftNum }100`, "Emily Moore" ],
+	[ `${ISoftNum }101`, "Matthew Wegener" ],
+	[ "sip:eepperson@isoftdata.onsip.com", "Emily Epperson" ],
+	[ `${ISoftNum }111`, "Tony Merritt" ],
+	[ `${ISoftNum }119`, "Gwyn Evans" ],
+	[ `${ISoftNum }301`, "Voicemail" ],
+	[ `${ISoftNum }102`, "Dayton Lowell" ],
+	[ `${ISoftNum }105`, "Dillon Sadofsky" ],
+	[ `${ISoftNum }108`, "Mark Hardisty" ],
+	[ `${ISoftNum }202`, "Web Dev" ],
+	[ `${ISoftNum }113`, "Brian Roy" ],
+	[ `${ISoftNum }118`, "Jordan Bonge" ],
+	[ "sip:vm.jchristensen@isoftdata.onsip.com", "Jake Christensen" ],
+	[ "sip:eehuntgroup@isoftdata.onsip.com", "EEhuntGroup" ],
+	[ "sip:htp.support.hunt.group@isoftdata.onsip.com", "Support Hunt Group" ],
+	[ "sip:vm.elowell@isoftdata.onsip.com", "E Lowell" ],
+	[ "sip:default.hunt@isoftdata.onsip.com", "Default Hunt" ],
+	[ "sip:vm.nfryzek@isoftdata.onsip.com", "N Fryzek" ],
+	[ "sip:presage.menu@isoftdata.onsip.com", "Presage Menu" ],
+	[ "sip:tech.support@isoftdata.onsip.com", "Tech Support" ],
+])
